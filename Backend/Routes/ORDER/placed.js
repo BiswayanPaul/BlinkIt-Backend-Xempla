f298403edff89bdf3123f914226b6164ba3c3a87
Abir Bera <abirbera2003@gmail.com>
2024-03-18 21:09:56 +0530

    retailer selection done

--- Backend/Routes/ORDER/placed.js
+++ Backend/Routes/ORDER/placed.js
@@ -11,7 +11,7 @@ function deg2rad(deg) {
 }
 
 function dist(lat1, lng1, lat2, lng2) {
-  const R = 6371; // Radius of the Earth in kilometers
+  const R = 6371000; // Radius of the Earth in kilometers
   const dLat = deg2rad(lat2 - lat1);
   const dLng = deg2rad(lng2 - lng1);
   const a =
@@ -25,35 +25,89 @@ function dist(lat1, lng1, lat2, lng2) {
   return distance;
 }
 
+function checkAvail(productList, order) {
+  const productsMap = new Map();
+  productList.forEach((product) => {
+    // console.log(`Product : ${product}`);
+    productsMap.set(product.p_id, true);
+  });
+  //   productList.forEach((product) => {
+  //     console.log(
+  //       ` CHK Product in map => ${product.p_id} : ${productsMap.get(
+  //         product.p_id
+  //       )} `
+  //     );
+  //   });
+
+  //   order.forEach((orderItem) => {
+  //     console.log(`Order Item => ${orderItem.p_id} : ${orderItem.amount}`);
+  //   });
+  for (const orderItem of order) {
+    if (!productsMap.has(orderItem.p_id)) {
+      console.log("false");
+      return false; // Product not found in productList
+    }
+    const pdct = productList.find((product) => product.p_id === orderItem.p_id);
+    // console.log(`Product in the store ${pdct.p_id} : ${pdct.amount}`);
+    if (pdct.amount < orderItem.amount) {
+      console.log("false in amount");
+      return false;
+    }
+  }
+  console.log("true");
+  return true;
+}
+
 router.post("/:id", async (req, res) => {
   const id = req.params.id;
-  console.log(`{id : ${id} }`);
+  console.log(`{ c_id : ${id} }`);
   var size;
   var order_id;
+  var retailer_id;
+  // c_id,r_id,shipping_address,order_details,
   try {
     size = await Order.countDocuments({});
     order_id = idString + (size + 1);
+    console.log(`{ o_id : ${order_id} }`);
   } catch (error) {
     res.status(500).send({ message: "DB Error" });
   }
   const latt = req.body.co_ordinates.latt;
   const long = req.body.co_ordinates.long;
-  var distance = [{}];
-
-  var retailer;
+  const orderedProductList = req.body.products;
+  var distance = new Map();
   try {
     const stores = await Store.find({});
-
-    stores.forEach((store) => {
-      console.log(store.r_id);
-    });
+    for (const store of stores) {
+      console.log(`Store : ${store.r_id}`);
+      if (await checkAvail(store.product_list, orderedProductList)) {
+        const store_loc = await Retailer.findOne(
+          { r_id: store.r_id },
+          { r_location: true }
+        );
+        // console.log(
+        //   `retailer found : ${store.r_id} : loc : ${store_loc.r_location}`
+        // );
+        const store_cord = store_loc.r_location.coordinates;
+        distance.set(
+          store.r_id,
+          await dist(store_cord[0], store_cord[1], latt, long)
+        );
+      }
+    }
+    const sortedDistanceArray = Array.from(distance).sort(
+      (a, b) => a[1] - b[1]
+    );
+    const sortedDistanceMap = new Map(sortedDistanceArray);
+    // console.log(sortedDistanceMap);
+    const mapAsArray = Array.from(sortedDistanceMap);
+    retailer_id = mapAsArray[0][0];
+    const mapJSON = JSON.stringify(Object.fromEntries(mapAsArray));
+    // console.log(`distance map : ${mapJSON}`);
+    res.status(200).json({ distance_map: mapJSON });
   } catch (error) {
     res.status(500).send({ message: "Stores DB Error" });
   }
-
-  console.log(order_id);
-  const orderedProductList = req.body.products;
-  res.status(200).send(req.co_ordinates);
 });
 
 module.exports = router;
