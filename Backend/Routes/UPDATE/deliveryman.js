322f991b4fb65305e7027973e683f116db443004
BiswayanPaul <biswayanpaul2015@gmail.com>
2024-03-20 00:15:57 +0530

    added the delivered route in update/deliveryman.js, didn't import the route in index.js, it's a completely separate section

--- Backend/Routes/UPDATE/deliveryman.js
+++ Backend/Routes/UPDATE/deliveryman.js
@@ -1,12 +1,39 @@
 const express = require('express')
-
+const DeliveryPerson = require('../../Model/deliveryPerson');
+const Order = require('../../Model/order');
 
 const deliveryManUpdate = express.Router();
 
-deliveryManUpdate.put("/", (req, res) => {
-    res.status(200).json({
-        msg: "Hello from deliveryman Update"
-    })
+deliveryManUpdate.put("/delivered/:orderID", (req, res) => {
+    const orderId = req.params.orderID;
+    const d_man_id = req.body.d_man_id;
+
+    const d_man = DeliveryPerson.findOne({ d_id: d_man_id });
+    const order = Order.findOne({
+        o_id: orderId,
+        payment_details:
+        {
+            status: "pending",
+            mode: "cash"
+        }
+    });
+
+    d_man.d_idle = true;
+
+    order.payment_details.status = 'paid';
+
+    order.delivery_status = "delivered";
+
+    const savedDman = d_man.save();
+    const savedOrder = order.save();
+
+    res
+        .status(200)
+        .json({
+            msg: "Order is delivered to customer successfully",
+            d_id: savedDman.d_id,
+            o_id: savedOrder.o_id
+        })
 })
 
 module.exports = {
